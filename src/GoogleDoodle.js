62467095c67d56ef63cc9add4aee076ec85f76b3
Tiffany <Owhyze1@gmail.com>
2021-06-02 12:54:59 -0400

    Working on google doodle

---
+++ src/GoogleDoodle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './GoogleDoodle.css';
+
+function GoogleDoodle() {
+  return <div className="googleDoodle">
+    <div className="doodle">
+      <h1>Google</h1>
+    </div>
+  </div>;
+}
+
+export default GoogleDoodle;
