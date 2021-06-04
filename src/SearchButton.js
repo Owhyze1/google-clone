ee5f6295175e730b25b077d1d03231e55677eeca
Tiffany <Owhyze1@gmail.com>
2021-06-04 16:42:20 -0400

    Create 2 search buttons

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
-// import Button from 'react';
 import './css/SearchButton.css';
 
 function SearchButton() {
-  return <div>Search Button</div>;
+  return <div className="button-div">
+      <button type="submit" value="Google Search">
+        Google Search
+      </button>
+      <button type="submit" value="I'm Feeling Lucky">
+        I'm Feeling Lucky
+      </button>
+    </div>;
 }
 
 export default SearchButton;
