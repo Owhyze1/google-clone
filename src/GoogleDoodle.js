8872617fd90213dee2bbee221d9b70e450df3522
Tiffany <Owhyze1@gmail.com>
2021-06-03 20:13:26 -0400

    Created div and className for each letter in "Google"

--- src/GoogleDoodle.js
+++ src/GoogleDoodle.js
@@ -4,9 +4,12 @@ import './GoogleDoodle.css';
 function GoogleDoodle() {
   return <div className="googleDoodle">
     <div className="doodle">
-      <div>
-        Google
-      </div>
+      <div className="first-g">G</div>
+      <div className="first-o">o</div>
+      <div className="second-o">o</div>
+      <div className="second-g">g</div>
+      <div className="l">l</div>
+      <div className="e">e</div>
     </div>
   </div>;
 }
