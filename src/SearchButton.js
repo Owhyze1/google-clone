8d6121385adb1ae98046d5793962666b7c9ac01f
Tiffany Wise <TiffanyJWise84@gmail.com>
2021-06-27 10:33:49 -0400

    Added div for each search button

--- src/SearchButton.js
+++ src/SearchButton.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './css/SearchButton.css';
 
 function SearchButton() {
-  return <div className="button-div">
-      <button type="submit" value="Google Search">
-        Google Search
-      </button>
-      <button type="submit" value="I'm Feeling Lucky">
-        I'm Feeling Lucky
-      </button>
+  return <div className="buttonContainer">
+      <div className="button-div-left">
+        <button type="submit" value="Google Search">
+          Google Search
+        </button>
+      </div>
+      <div className="button-div-right">
+        <button type="submit" value="I'm Feeling Lucky">
+          I'm Feeling Lucky
+        </button>
+      </div>
     </div>;
 }
-
 export default SearchButton;
